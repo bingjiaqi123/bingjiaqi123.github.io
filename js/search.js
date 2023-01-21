e392fe59739597609be137c6d30d21139151830c
bingjiaqi123 <bingjiaqi2022@163.com>
2023-01-21 16:51:55 +0800

    试一下algolia

--- js/search.js
+++ js/search.js
@@ -1,111 +1,81 @@
-
-
-summaryInclude=60;
-var fuseOptions = {
-shouldSort: true,
-includeMatches: true,
-threshold: 0.0,
-tokenize:true,
-location: 0,
-distance: 100,
-maxPatternLength: 32,
-minMatchCharLength: 1,
-keys: [
-{name:"title",weight:0.8},
-{name:"contents",weight:0.5},
-{name:"tags",weight:0.3},
-{name:"categories",weight:0.3}
-]
-};
-
-
-var searchQuery = param("s");
-if(searchQuery){
-$("#search-query").val(searchQuery);
-executeSearch(searchQuery);
-}else {
-$('#search-results').append("<p>Please enter a word or phrase above</p>");
-}
-
-
-
-function executeSearch(searchQuery){
-$.getJSON( "/index.json", function( data ) {
-var pages = data;
-var fuse = new Fuse(pages, fuseOptions);
-var result = fuse.search(searchQuery);
-console.log({"matches":result});
-if(result.length > 0){
-populateResults(result);
-}else{
-$('#search-results').append("<p>No matches found</p>");
-}
+// 构建搜索客户端
+const searchClient = algoliasearch('Application_ID', 'Search-Only_API_Key');
+// 构建索引搜索
+const search = instantsearch({
+  indexName: 'Index_Name',
+  searchClient,
 });
-}
 
-function populateResults(result){
-$.each(result,function(key,value){
-var contents= value.item.contents;
-var snippet = "";
-var snippetHighlights=[];
-var tags =[];
-if( fuseOptions.tokenize ){
-snippetHighlights.push(searchQuery);
-}else{
-$.each(value.matches,function(matchKey,mvalue){
-if(mvalue.key == "tags" || mvalue.key == "categories" ){
-snippetHighlights.push(mvalue.value);
-}else if(mvalue.key == "contents"){
-start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-snippet += contents.substring(start,end);
-snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
-}
-});
-}
+//展示数据模版
+var hitTemplate =
+  '<article class="post">' +
+  '<h2 class="post-title"><a href="{{url}}">{{{_highlightResult.title.value}}}</a></h2>' +
+  '{{{_highlightResult.summary.value}}}' +
+  '</article>';
 
-    if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
+//无数据模版
+var noResultsTemplate =
+  '<div class="text-center">没有找到 <strong>{{query}}</strong> 相关的结果</div>';
+
+//配置添加小部件
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    placeholder: '请输入搜索内容……',
+    autofocus: true,
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: noResultsTemplate,
+      item: hitTemplate
     }
-    //pull template from hugo templarte definition
-    var templateDefinition = $('#search-result-template').html();
-    //replace values
-    var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.permalink,tags:value.item.tags,categories:value.item.categories,snippet:snippet});
-    $('#search-results').append(output);
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+]);
 
-    $.each(snippetHighlights,function(snipkey,snipvalue){
-      $("#summary-"+key).mark(snipvalue);
-    });
-
+//启动搜索
+search.start();// 构建搜索客户端
+const searchClient = algoliasearch('Application_ID', 'Search-Only_API_Key');
+// 构建索引搜索
+const search = instantsearch({
+  indexName: 'Index_Name',
+  searchClient,
 });
-}
 
-function param(name) {
-return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
-}
+//展示数据模版
+var hitTemplate =
+  '<article class="post">' +
+  '<h2 class="post-title"><a href="{{url}}">{{{_highlightResult.title.value}}}</a></h2>' +
+  '{{{_highlightResult.summary.value}}}' +
+  '</article>';
 
-function render(templateString, data) {
-var conditionalMatches,conditionalPattern,copy;
-conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
-//since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
-copy = templateString;
-while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
-if(data[conditionalMatches[1]]){
-//valid key, remove conditionals, leave contents.
-copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
-}else{
-//not valid, remove entire section
-copy = copy.replace(conditionalMatches[0],'');
-}
-}
-templateString = copy;
-//now any conditionals removed we can do simple substitution
-var key, find, re;
-for (key in data) {
-find = '\\$\\{\\s*' + key + '\\s*\\}';
-re = new RegExp(find, 'g');
-templateString = templateString.replace(re, data[key]);
-}
-return templateString;
-}
+//无数据模版
+var noResultsTemplate =
+  '<div class="text-center">没有找到 <strong>{{query}}</strong> 相关的结果</div>';
 
+//配置添加小部件
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    placeholder: '请输入搜索内容……',
+    autofocus: true,
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: noResultsTemplate,
+      item: hitTemplate
+    }
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+]);
+
+//启动搜索
+search.start();
